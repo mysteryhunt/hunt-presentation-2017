60aaef6513408c017d6f6b6c24722a6b888b4aa8
tkfocht <tk.focht@gmail.com>
2016-10-09 01:51:25 -0700

    Move Google API key to config, clean up location.location.location

--- default_config.py
+++ default_config.py
@@ -1,6 +1,7 @@
 SECRET_KEY = "9dsj38gj3j9fcjsaojfADf8"
 CUBE_API_SERVICE = "http://localhost:8182"
 SUBMIT_URL = "/submit/puzzle/%s"
+GOOGLE_API_KEY = "AIzaSyCDkIrVkWdEaOqWe2tPRRmspwL26pU71GI"
 AWS_ASSET_BUCKET = "eastern-toys-assets"
 AWS_ASSET_ACCESS_KEY = "AKIAIAR4CIFKO4DGX4WQ"
 AWS_ASSET_SECRET_KEY = "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"
