76678215309613ba554b154c36b7af4f312690d0
tkfocht <tk.focht@gmail.com>
2016-12-12 01:17:24 -0800

    Remove ports from run_admin and run_submit, these were improperly committed

--- run_submit.py
+++ run_submit.py
@@ -12,4 +12,4 @@ from common import login
 app.register_blueprint(login.blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=7000)
+    app.run(debug=True, host="0.0.0.0")
