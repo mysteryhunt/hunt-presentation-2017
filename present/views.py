4b5a85c2ebbcba238fbe670e8bba11340ac90826
tkfocht <tk.focht@gmail.com>
2016-12-16 03:32:06 -0800

    Finish round/puzzle map

--- present/views.py
+++ present/views.py
@@ -50,7 +50,19 @@ ROUND_PUZZLE_MAP = {
   'index': ['dynast','rescue_the_linguist','dungeon','rescue_the_economist','thespians',\
             'rescue_the_chemist','bridge','criminal','minstrels','cube','warlord','merchants','fortress'],
   'fighter': ['f' + str(i) for i in range(1,11+1)],
-  'wizard': ['w' + str(i) for i in range(1,11+1)]
+  'wizard': ['w' + str(i) for i in range(1,11+1)],
+  'cleric': ['cl-l' + str(i) for i in range(1,5+1)] + ['cl-r' + str(i) for i in range(1,5+1)],
+  'linguist': ['l' + str(i) for i in range(1,12+1)],
+  'economist': ['e' + str(i) for i in range(1,8+1)],
+  'chemist': ['ch' + str(i) for i in range(1,9+1)],
+  'dynast': ['dynast' + str(i) for i in range(1,11+1)],
+  'dungeon': ['dungeon' + str(i) for i in range(1,11+1)],
+  'thespians': ['thespians-l' + str(i) for i in range(1,5+1)] + ['thespians-r' + str(i) for i in range(1,5+1)],
+  'bridge': ['bridge' + str(i) for i in range(1,16+1)],
+  'criminal': ['criminal' + str(i) for i in range(1,10+1)],
+  'minstrels': ['minstrels' + str(i) for i in range(1,7+1)],
+  'cube': ['cube' + str(i) for i in range(1,8+1)],
+  'warlord': ['warlord-' + direction for direction in ['nw','nc','ne','cw','cc','ce','sw','sc','se']]
 }
     
 def get_core_display_data():
