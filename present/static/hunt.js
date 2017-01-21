a0ce995604730ce6a710709fa0622a000ee13e8a
Matt Gruskin <matthew.gruskin@gmail.com>
2017-01-21 16:04:29 +0000

    add a client-side javascript answer checker into the solutions site

--- present/static/hunt.js
+++ present/static/hunt.js
@@ -2,4 +2,37 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip({
     template: '<div class="tooltip"><div class="tooltip-inner"></div></div>'
   });
+
+  var answerHashCodes = {};
+  $.getJSON("/static/answer_hash_codes.json", function(data) {
+    answerHashCodes = data;
+  });
+
+  $("#checkAnswerTextbox").on("change keyup paste", function(event) {
+    var answerHashCode = answerHashCodes[puzzleId];
+    if (answerHashCode === undefined) {
+      $("#checkAnswerTextbox").css("background-color", "white");
+      return;
+    }
+
+    var submission = $("#checkAnswerTextbox").val();
+    if (!submission) {
+      $("#checkAnswerTextbox").css("background-color", "white");
+      return;
+    }
+
+    var strippedSubmission = submission.toUpperCase().replace(/[^A-Z0-9]/g, "");
+    var submissionHashCode = 0;
+    for (var i = 0; i < strippedSubmission.length; i++) {
+      var char = strippedSubmission.charCodeAt(i);
+      submissionHashCode = ((submissionHashCode << 5) - submissionHashCode) + char;
+      submissionHashCode = submissionHashCode & submissionHashCode;
+    }
+
+    if (submissionHashCode == answerHashCode) {
+      $("#checkAnswerTextbox").css("background-color", "#80ff80");
+    } else {
+      $("#checkAnswerTextbox").css("background-color", "#ff8080");
+    }
+  });
 })
